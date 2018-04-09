0bc561bac483e8750228f8cce9b52d5c16406f53
Solenomnom <soledad.cot@epitech.eu>
2018-04-09 04:34:01 +0200

    arrow

---
+++ TestHolotoolkit/Assets/Scripts/RecipeLeftArrow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class RecipeLeftArrow : MonoBehaviour {
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        //print("oninputcliked left");
+       GameObject.Find("RecipeManager").GetComponent<RecipeManager>().prevStep();
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ TestHolotoolkit/Assets/Scripts/RecipeRightArrow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class RecipeRightArrow : MonoBehaviour, IInputClickHandler {
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+       // print("oninputcliked right");
+
+        GameObject.Find("RecipeManager").GetComponent<RecipeManager>().nextStep();
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

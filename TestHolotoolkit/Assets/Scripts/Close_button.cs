1e8a89f820771f6d81244039f81a13019b076424
KothLeRoux <romain.chateigner@epitech.eu>
2018-05-24 19:21:05 +0900

    Error gestion

--- TestHolotoolkit/Assets/Scripts/Close_button.cs
+++ TestHolotoolkit/Assets/Scripts/Close_button.cs
@@ -5,21 +5,20 @@ using HoloToolkit.Unity.InputModule;
 
 public class Close_button : MonoBehaviour, IInputClickHandler
 {
-    public void OnInputClicked(InputClickedEventData eventData)
-    {
-        GameObject.Find("Error Popup").SetActive(false);
-        GameObject.Find("Missing Email").SetActive(false);
-        GameObject.Find("Missing Username").SetActive(false);
-        GameObject.Find("Missing Password").SetActive(false);
-    }
 
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        GameObject.Find("Error Popup").SetActive(false);
+    }
+
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }

800324cd9e9eff691d2e1251969b35ca301f1759
Solenomnom <soledad.cot@epitech.eu>
2018-05-25 10:01:29 +0200

    merge

---
+++ TestHolotoolkit/Assets/Scripts/DisconnectButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using System;
+using UnityEngine.SceneManagement;
+
+public class DisconnectButton : MonoBehaviour, IInputClickHandler
+{
+    GameObject camera;
+    GameObject clientManager;
+    GameObject inputManager;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+
+        //supprimer tous les fichiers du dossier
+        ClientManager.CM.setUserInfo(null);
+        Destroy(camera);
+        Destroy(clientManager);
+        Destroy(inputManager);
+        SceneManager.LoadScene(2);
+    }
+
+    // Use this for initialization
+    void Start () {
+        camera = GameObject.Find("MixedRealityCameraParent");
+        clientManager = GameObject.Find("ClientManager");
+        inputManager = GameObject.Find("InputManager");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ TestHolotoolkit/Assets/Scripts/HomeButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class HomeButton : MonoBehaviour, IInputClickHandler
+{
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        ClientManager.CM.LoadMenuScene();
+    }
+        // Use this for initialization
+        void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
